3738903cb04fe43a0cecc601e63994b3f76f5412
KYS <90492881+luminous77777@users.noreply.github.com>
2025-05-19 15:57:27 +0900

    부트스트랩 공부

--- tom9_test/src/main/java/controller/SignupServlet.java
+++ tom9_test/src/main/java/controller/SignupServlet.java
@@ -2,12 +2,13 @@ package controller;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import domain.Member;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import service.MemberService;
 
 @WebServlet("/signup")
